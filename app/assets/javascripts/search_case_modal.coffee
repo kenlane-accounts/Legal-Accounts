deb638b4e31cf6582b0a3812f0b561efee455e55
corsair <alezander82@mail.ru>
2018-06-15 20:57:05 +0300

    case search by ref, description, client lastname

--- app/assets/javascripts/search_case_modal.coffee
+++ app/assets/javascripts/search_case_modal.coffee
@@ -2,7 +2,7 @@ class SearchCaseModal
   init: (e)->
     modal = $('#search_case_modal')
 
-    $('#search_case_reference').on 'railsAutocomplete.select', (event, data)->
+    $('#search_case_term').on 'railsAutocomplete.select', (event, data)->
       case_id = data.item.id
       if case_id
         window.location.href = "/cases/#{case_id}"
--- app/controllers/search_case_controller.rb
+++ app/controllers/search_case_controller.rb
@@ -1,6 +1,7 @@
 class SearchCaseController < ApplicationController
   def search
-    @cases = Case.where('reference like ?', "%#{params[:search_case_reference]}%")
+    @cases = Case.includes(:client).references(:clients).where 'cases.reference like :term OR cases.description like :term OR clients.lastname like :term',
+                        term: "%#{params[:search_case_term]}%"
     # render :json => @cases.map{ |c| { :id => c.id, :reference => c.reference } }
   end
 end
